6b19f4055b2ad3f232df219b5f111ec1e7f2dd91
Bohdan <stetsenko.bogdan.work@gmail.com>
2023-01-04 00:48:41 +0200

    first commit

---
+++ client/src/components/userDetails.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export default class UserDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: "",
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:8080/userData", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        token: window.localStorage.getItem("token"),
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userData");
+        this.setState({ userData: data.data });
+      });
+  }
+  render() {
+    return (
+      <div>
+        Name<h1>{this.state.userData.fname}</h1>
+        Surname<h1>{this.state.userData.lname}</h1>
+        Email <h1>{this.state.userData.email}</h1>
+        Group <h1>IV-93</h1>
+      </div>
+    );
+  }
+}
